dc0571630419926e693758464871ebc40c23f195
Zyhru <zyhrups3@gmail.com>
2024-08-26 22:24:01 -0400

    my config (prime inspo)

---
+++ lua/zyhru/init.lua
@@ -0,0 +1,4 @@
+-- sources the lua files everytime i open up vim
+require("zyhru.set")
+require("zyhru.remap")
+require("zyhru.lazy_init")
---
+++ lua/zyhru/remap.lua
@@ -0,0 +1,3 @@
+vim.g.mapleader = " "
+-- while in normal mode, if space is pressed then execute the Ex command
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex) 
